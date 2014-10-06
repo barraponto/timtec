aae8c31a7c0a465d77f3f8d2a2d37e24dc0a2317
Capi Etheriel <barraponto@gmail.com>
2014-10-06 18:38:07 -0300

    let users see each other profiles

--- enoisoverrides/views.py
+++ enoisoverrides/views.py
@@ -1,11 +1,26 @@
 from django.views.generic.list import ListView
-from accounts.views import ProfileView
+from django.views.generic.detail import DetailView
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from accounts.models import TimtecUser
 
 
-class EnoisProfileView(ProfileView):
+class EnoisProfileView(DetailView):
+    model = get_user_model()
+    template_name = 'profile.html'
+    context_object_name = 'profile_user'
+
+    def get_object(self):
+        if hasattr(self, 'kwargs') and 'username' in self.kwargs:
+            try:
+                return get_object_or_404(self.model, username=self.kwargs['username'])
+            except:
+                return self.request.user
+        else:
+            return self.request.user
+
     def get_context_data(self, **kwargs):
-        context = super(ProfileView, self).get_context_data()
+        context = super(DetailView, self).get_context_data()
         context['portfolios'] = context['profile_user'].portfolio_set.all()[:4]
         return context
 
