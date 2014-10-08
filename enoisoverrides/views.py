e6ce5b8811b687b53982ff8890dafd520f425d04
Capi Etheriel <barraponto@gmail.com>
2014-10-08 02:18:01 -0300

    display courses taken or enrolled in profile. fixes #28

--- enoisoverrides/views.py
+++ enoisoverrides/views.py
@@ -22,6 +22,12 @@ class EnoisProfileView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(DetailView, self).get_context_data()
         context['portfolios'] = context['profile_user'].portfolio_set.all()[:4]
+        context['courses_given'] = (
+            cp.course for cp in
+            context['profile_user'].courseprofessor_set.all()[:4])
+        context['courses_enrolled'] = (
+            cp.course for cp in
+            context['profile_user'].coursestudent_set.all()[:4])
         return context
 
 
